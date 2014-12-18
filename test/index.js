eaaf2d53a385d6b13fcff33734675c3d886b5b51
Calvin Metcalf <cmetcalf@appgeo.com>
2014-12-18 16:31:43 -0500

    varient encoding of password protected keys

--- test/index.js
+++ test/index.js
@@ -39,6 +39,34 @@ var dsa2 = {
 	private: fs.readFileSync(__dirname + '/dsa.2048.priv'),
 	public: fs.readFileSync(__dirname + '/dsa.2048.pub')
 };
+var dsapass = {
+	private: {
+		key:fs.readFileSync(__dirname + '/pass.dsa.1024.priv'),
+		passphrase:'password'
+	},
+	public: fs.readFileSync(__dirname + '/pass.dsa.1024.pub')
+};
+var dsapass2 = {
+	private: {
+		key:fs.readFileSync(__dirname + '/pass2.dsa.1024.priv'),
+		passphrase:'password'
+	},
+	public: fs.readFileSync(__dirname + '/pass2.dsa.1024.pub')
+};
+var rsapass = {
+	private: {
+		key:fs.readFileSync(__dirname + '/pass.rsa.1024.priv'),
+		passphrase:'password'
+	},
+	public: fs.readFileSync(__dirname + '/pass.rsa.1024.pub')
+};
+var rsapass2 = {
+	private: {
+		key:fs.readFileSync(__dirname + '/pass.rsa.2028.priv'),
+		passphrase:'password'
+	},
+	public: fs.readFileSync(__dirname + '/pass.rsa.2028.pub')
+};
 function isNode10() {
   return process.version && process.version.split('.').length === 3 && parseInt(process.version.split('.')[1], 10) <= 10;
 }
@@ -105,6 +133,22 @@ testIt(nonrsa1024, new Buffer('sha512 with 1024 keys non-rsa key'), 'RSA-SHA512'
 testIt(rsa2028, new Buffer('SHA512 with 2028 keys'), 'RSA-SHA512');
 if (!isNode10()) {
 	ectestIt(ecpass, new Buffer('ecdsa with password'), 'ecdsa-with-SHA1');
+	ectestIt(dsapass, new Buffer('dsa with 1024 keys and a password'), 'DSA-SHA');
+	ectestIt(dsapass2, new Buffer('dsa with 1024 keys and a password varient'), 'DSA-SHA');
+	testIt(rsapass, new Buffer('sha1 with 1024 keys and password, varient'), 'RSA-SHA1');
+	testIt(rsapass2, new Buffer('sha1 with 2024 keys and password, varient'), 'RSA-SHA1');
+	testIt(rsapass, new Buffer('sha224 with 1024 keys and password, varient'), 'RSA-SHA224');
+	testIt(rsapass2, new Buffer('sha224 with 2024 keys and password, varient'), 'RSA-SHA224');
+	testIt(rsapass, new Buffer('sha256 with 1024 keys and password, varient'), 'RSA-SHA256');
+	testIt(rsapass2, new Buffer('sha256 with 2024 keys and password, varient'), 'RSA-SHA256');
+	testIt(rsapass, new Buffer('sha384 with 1024 keys and password, varient'), 'RSA-SHA384');
+	testIt(rsapass2, new Buffer('sha384 with 2024 keys and password, varient'), 'RSA-SHA384');
+	testIt(rsapass, new Buffer('sha512 with 1024 keys and password, varient'), 'RSA-SHA512');
+	testIt(rsapass2, new Buffer('sha512 with 2024 keys and password, varient'), 'RSA-SHA512');
+	testIt(rsapass, new Buffer('rmd160 with 1024 keys and password, varient'), 'RSA-RIPEMD160');
+	testIt(rsapass2, new Buffer('rmd160 with 2024 keys and password, varient'), 'RSA-RIPEMD160');
+	testIt(rsapass, new Buffer('md5 with 1024 keys and password, varient'), 'RSA-MD5');
+	testIt(rsapass2, new Buffer('md5 with 2024 keys and password, varient'), 'RSA-MD5');
 	testIt(pass1024, new Buffer('sha1 with 1024 keys and password'), 'RSA-SHA1');
 	testIt(pass1024, new Buffer('sha224 with 1024 keys and password'), 'RSA-SHA224');
 	testIt(pass1024, new Buffer('sha256 with 1024 keys and password'), 'RSA-SHA256');
